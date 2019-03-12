fc16ac6ebf039fe3b2a0a5814cec007e6b57e73d
surf2018 <ecuster@126.com>
2019-03-12 19:18:14 +0800

    front:myfirstPro

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,6 +1,48 @@
 <template>
 <div class="body-status">
-  <p>{{current_user}}登录成功</p>
+    <el-row>
+      <el-col :span="4"><div class="grid-content bg-purple-light">{{current_user}}登录成功</div></el-col>
+    </el-row>
+    <div>
+      <el-menu
+        default-active="2"
+        class="index-menu"
+        @open="handleOpen"
+        @close="handleClose">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+      <div class="index-content">
+      </div>
+  </div>
 </div>
 </template>
 <script >
@@ -38,11 +80,12 @@
     border: 1px solid #a0b1c4;
   }
 
-  .login-title {
-    text-align: center;
+  .index-menu {
+    width: 20%;
+    top:20px;
   }
-  .el-form.login-ruleForm {
-    margin-bottom:auto
+  .index-content {
+    width: 80%;
   }
 .body-status {
   margin-left: auto;
