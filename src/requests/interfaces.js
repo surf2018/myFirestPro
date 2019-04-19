4875a55bc67c993c1cc1ea4780e4a707539e6e31
surf2018 <ecuster@126.com>
2019-04-19 17:59:09 +0800

    front:myfirstPro

--- src/requests/interfaces.js
+++ src/requests/interfaces.js
@@ -3,23 +3,20 @@ const interface_url='interfaces/imp/';
 
 
 //更新接口
-export const update_interface = function (interface_id,name,desp,parent) {
-  return put_code(interface_url+interface_id,{"name":name,"description":desp,"parent":parent})
+export const update_interface = function (interface_id,data) {
+  return put_code(interface_url+interface_id,data)
 };
 //创建接口
-export const create_interface= function(){
-  return post_code(interface_url,{"name":name,"description":desp,"parent":parent})
+export const create_interface= function(data){
+  return post_code(interface_url,data)
 };
-
+//获取接口
 export const get_interfaces = function(){
   return get_code(interface_url)
-
 };
+//删除接口
 export const del_interface = function(interface_id){
   return del_code(interface_url+interface_id)
 
 };
-export const get_interface=function (interface_id) {
-  return get_code(interface_url+interface_id)
 
-}
